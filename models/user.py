9f39ca54ea5e695e93a36370b66559ee9ad2d75d
Allan De Guzman <abeydeguzman@gmail.com>
2024-09-11 06:31:37 +0800

    Added user model

---
+++ models/user.py
@@ -0,0 +1,16 @@
+class User:
+
+    def __init__(self, id, email, username, password):
+        self.id = id
+        self.email = email
+        self.username = username
+        self.password = password
+
+    def qrCode(self):
+        pass
+
+    def sync(self):
+        pass
+
+    def upload(self):
+        pass
